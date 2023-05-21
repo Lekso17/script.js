0f1cb7f59cc6c8fd1f22e754e52759ee7ec76f19
Lekso17 <129981401+Lekso17@users.noreply.github.com>
2023-05-21 19:45:04 +0400

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,25 @@
+    //   variables 
+    let firstName = "Lekso";
+    let lastName = "Mdinaradze";
+    let age = 26;
+
+   // sentences
+   console.log(`my name is ${firstName} ${lastName} i am ${age} years old  `)
+   console.log("my name is " + firstName + " " + lastName + " i am " + age + " years old")
+
+// operations
+    let x = 12;
+    let y = 8 ;
+    let z = 13
+   
+    console.log( x + y);
+    console.log( x - y);
+    console.log( x * z);
+    console.log( z / y );
+    
+     
+        
+    // rectangle
+    let width = 5 ;
+    let lenght = 7 ;
+    console.log( width * lenght)
